2904749e3e679160c89b516e377b1354842e3438
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-08-29 15:01:52 +0530

    Update Implementation_of_SJF.java

--- Implementation_of_SJF.java
+++ Implementation_of_SJF.java
@@ -13,3 +13,14 @@ public class Lab_1_SJF {
         int flag[] = new int[n];  
         int system_time = 0, total = 0;
         float avg_wait = 0;
+        
+        for(int i = 0; i < n; i++){
+            System.out.println("Enter process " + (i + 1) + " arrival time: ");
+            arrival[i] = sc.nextInt();
+
+            System.out.println("Enter process " + (i + 1) + " brust time: ");
+            burst_time[i] = sc.nextInt();
+
+            process_id[i] = i + 1;
+            flag[i] = 0;
+        }

17eaeb0f0cd5668b0510aa7cdeaf1ac3a5e1d6c5
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-08-29 23:55:07 +0530

    Update Implementation_of_RR.java

--- Implementation_of_RR.java
+++ Implementation_of_RR.java
@@ -40,3 +40,10 @@ public class Lab_1_RR {
             if(n == count)
             break;
         }
+        
+        System.out.print("\nProcess\tBurst Time\tWaiting Time\n");
+        for(i = 0; i < n; i++){
+            wait[i] = turn_around[i] - burst_time[i];
+            avg_wait = avg_wait + wait[i];
+            System.out.print("\n  " + (i + 1) + "\t  " + burst_time[i] + "\t\t   " + wait[i]);
+        }

b711f77be92eb51a64f5a9564d6b6d8589ef840b
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-08-29 23:54:11 +0530

    Update Implementation_of_RR.java

--- Implementation_of_RR.java
+++ Implementation_of_RR.java
@@ -1,1 +1,9 @@
-
+mport java.util.*;
+public class Lab_1_RR {
+    public static void main(String args[]){
+        int n, i, time_slice, count = 0, temp, sq = 0, burst_time[], wait[], turn_around[], rem_bt[];
+        float avg_wait = 0;
+        burst_time = new int[10];
+        wait = new int[10];
+        turn_around = new int[10];
+        rem_bt = new int[10];

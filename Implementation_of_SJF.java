97724c0f16e47031530920bf5cf0773964875cf9
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-08-29 15:01:31 +0530

    Update Implementation_of_SJF.java

--- Implementation_of_SJF.java
+++ Implementation_of_SJF.java
@@ -1,1 +1,15 @@
-
+import java.util.*;
+public class Lab_1_SJF {  
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println ("Enter the number of process:");
+        int n = sc.nextInt();
+        int process_id[] = new int[n];
+        int arrival[] = new int[n];
+        int burst_time[] = new int[n]; 
+        int turn_around[] = new int[n];
+        int completion_time[] = new int[n]; 
+        int wait[] = new int[n];  
+        int flag[] = new int[n];  
+        int system_time = 0, total = 0;
+        float avg_wait = 0;

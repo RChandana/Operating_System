b28649370e77edddf7b0c96f34f0c4a0de13ee37
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-08-29 23:54:46 +0530

    Update Implementation_of_RR.java

--- Implementation_of_RR.java
+++ Implementation_of_RR.java
@@ -17,3 +17,26 @@ public class Lab_1_RR {
             burst_time[i] = sc.nextInt();
             rem_bt[i] = burst_time[i];
         }
+        
+        System.out.print("Enter Time Slice = ");
+        time_slice = sc.nextInt();
+        while(true){
+            for (i = 0, count = 0; i < n; i++){
+                temp = time_slice;
+                if(rem_bt[i] == 0){
+                    count++;
+                    continue;
+                }
+                if(rem_bt[i] > time_slice)
+                rem_bt[i] = rem_bt[i] - time_slice;
+                else
+                if(rem_bt[i] >= 0){
+                    temp = rem_bt[i];
+                    rem_bt[i] = 0;
+                }
+                sq = sq + temp;
+                turn_around[i] = sq;
+            }
+            if(n == count)
+            break;
+        }

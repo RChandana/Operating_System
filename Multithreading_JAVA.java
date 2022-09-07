c163dd08d842d4ce1309b2a6cf5e8ccd4776c1c8
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-09-07 16:10:05 +0530

    Update Multithreading_JAVA.java

--- Multithreading_JAVA.java
+++ Multithreading_JAVA.java
@@ -1,2 +1,14 @@
 // Implementation of Multithreading using JAVA.
+import java.lang.Thread;
+import java.util.*;
+class Multithread extends Thread{
+   public void run(){
+       try {
+           System.out.println("Thread " + Thread.currentThread().getId() + " is running");
+       }
+       catch (Exception e) {
+           System.out.println("Exception");
+       }
+   }
+}
 

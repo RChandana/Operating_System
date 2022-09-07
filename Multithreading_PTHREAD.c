97fde0f7134fadef3c363447a9488a02c08c4471
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-09-07 16:11:49 +0530

    Update Multithreading_PTHREAD.c

--- Multithreading_PTHREAD.c
+++ Multithreading_PTHREAD.c
@@ -11,3 +11,12 @@ void *myThread(void *vargp)
     return NULL; 
 } 
 
+int main() 
+{ 
+    pthread_t thread_id; 
+    printf("Before Thread\n"); 
+    pthread_create(&thread_id, NULL, myThread, NULL); 
+    pthread_join(thread_id, NULL); 
+    printf("After Thread\n"); 
+    exit(0); 
+}

b7923e4b358cefc4ad2a968115cfb369b58e0005
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-09-07 16:11:31 +0530

    Update Multithreading_PTHREAD.c

--- Multithreading_PTHREAD.c
+++ Multithreading_PTHREAD.c
@@ -1,2 +1,13 @@
 // Implementation of Multithreading using PTHREAD
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <pthread.h> 
+  
+void *myThread(void *vargp) 
+{ 
+    sleep(1); 
+    printf("Inside Thread \n"); 
+    return NULL; 
+} 
 

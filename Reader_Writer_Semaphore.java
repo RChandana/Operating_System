67f21ff1721bd4de9b444fb908de9e17dd0e498a
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-10-04 17:17:53 +0530

    Update Reader_Writer_Semaphore.java

--- Reader_Writer_Semaphore.java
+++ Reader_Writer_Semaphore.java
@@ -35,3 +35,18 @@ class ReaderWritersProblem {
             }
         }
     }
+
+    static class Write implements Runnable {
+        @Override
+        public void run() {
+            try {
+                writeLock.acquire();
+                System.out.println("Thread "+ Thread.currentThread().getName() + " is WRITING");
+                Thread.sleep(2500);
+                System.out.println("Thread "+ Thread.currentThread().getName() + " has finished WRITING");
+                writeLock.release();
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }

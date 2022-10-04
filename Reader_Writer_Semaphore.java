2df66e046013241739c6490cc333482438d6dec4
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-10-04 17:17:34 +0530

    Update Reader_Writer_Semaphore.java

--- Reader_Writer_Semaphore.java
+++ Reader_Writer_Semaphore.java
@@ -1,1 +1,37 @@
+import java.util.concurrent.Semaphore;
+class ReaderWritersProblem {
 
+    static Semaphore readLock = new Semaphore(1);
+    static Semaphore writeLock = new Semaphore(1);
+    volatile static int readCount = 0;
+
+    static class Read implements Runnable {
+        @Override
+        public void run() {
+            try {
+                readLock.acquire();
+                synchronized(ReaderWritersProblem.class) {
+                    readCount++;
+                }
+                if (readCount == 1) {
+                    writeLock.acquire();
+                }
+                readLock.release();
+
+                System.out.println("Thread "+Thread.currentThread().getName() + " is READING");
+                Thread.sleep(1500);
+                System.out.println("Thread "+Thread.currentThread().getName() + " has FINISHED READING");
+
+                readLock.acquire();
+                synchronized(ReaderWritersProblem.class) {
+                    readCount--;
+                }
+                if(readCount == 0) {
+                    writeLock.release();
+                }
+                readLock.release();
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }

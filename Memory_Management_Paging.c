609eaf68567867e8856675ea971c5f72b90778d9
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-10-22 22:24:07 +0530

    Update Memory_Management_Paging.c

--- Memory_Management_Paging.c
+++ Memory_Management_Paging.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
+int main(){
+    int ms, ps, nop, np, rempages, i, j, x, y, pa, offset;
+    int s[10], fno[10][20];
+    clrscr();
+    printf("\nEnter the memory size -- ");
+    scanf("%d",&ms);
 
+    printf("\nEnter the page size -- ");
+    scanf("%d", &ps);
+
+    nop = ms/ps;
+    printf("\nThe no. of pages available in memory are -- %d ", nop);
+    printf("\nEnter number of processes -- ");
+    scanf("%d", &np);

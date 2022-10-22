aeac3ca005378ab41bcf79cdb33728a885cc1f15
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-10-22 22:13:14 +0530

    Update Segmentation.java

--- Memory_Management_Segmentation/Segmentation.java
+++ Memory_Management_Segmentation/Segmentation.java
@@ -1,1 +1,19 @@
-
+package segmentation;
+public class Segment {
+	long size;
+	String unit;
+	long base;
+	long limit;
+	String limit_Unit;
+	public Segment() {
+		this.size = -1;
+		this.unit = null;
+		this.base = -1;
+		this.limit = -1;
+	}
+	public void clear_All() {
+		size = -1;
+		unit = null;
+		base = -1;
+		limit = -1;
+	}

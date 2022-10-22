20aa98e316fbbd0b475bf3959b05f94a28418d1f
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-10-22 22:26:54 +0530

    Update IPC_Pipes.c

--- IPC_Pipes.c
+++ IPC_Pipes.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
+#include<unistd.h>
 
+int main() {
+   int pipe_fds[2];
+   int return_status;
+   int pid;
+   char write_messages[2][20]={"Hi", "Hello"};
+   char read_message[20];
+   return_status = pipe(pipe_fds);
+   if (return_status == -1) {
+      printf("Unable to create pipe\n");
+      return 1;
+   }

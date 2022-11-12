0a71a015d3324a19aca8d7dbc6b57e9ddd79d531
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-11-12 21:19:08 +0530

    Update Least_Recently_Used_Page_Replacement.java

--- Least_Recently_Used_Page_Replacement.java
+++ Least_Recently_Used_Page_Replacement.java
@@ -1,1 +1,16 @@
-
+import java.util.*; 
+    class Least_Recently_Used {
+        int p[], n, fr[], m, fs[], index, k, l, flag_1 = 0, flag_2 = 0, page_faults = 0, frame_size = 3, i, j; 
+        Scanner sc = new Scanner(System.in); 
+        void read() { 
+            System.out.println("Enter page table size : "); 
+            n = sc.nextInt(); 
+			p = new int[n]; 
+            System.out.println("Enter the Reference String : "); 
+            for(int i = 0; i < n; i++) 
+            p[i] = sc.nextInt(); 
+            System.out.println("Enter the Number of frames : "); 
+            m = sc.nextInt(); 
+            fr = new int[m]; 
+            fs = new int[m]; 
+        }

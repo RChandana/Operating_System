c90e1ff96944a3112299d0fc581a3365a329d286
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2023-01-01 13:32:33 +0530

    Update Page_Replacement_FCFS.java

--- Page_Replacement_FCFS.java
+++ Page_Replacement_FCFS.java
@@ -1,1 +1,16 @@
-
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+class Test{
+    static int page_Faults(int pages[], int n, int capacity){
+        HashSet<Integer> s = new HashSet<>(capacity);
+        Queue<Integer> indexes = new LinkedList<>() ;
+        int page_faults = 0;
+        for (int i = 0; i < n; i++){
+            if (s.size() < capacity){
+                if (!s.contains(pages[i])){
+                    s.add(pages[i]);
+                    page_faults++;
+                    indexes.add(pages[i]);
+                }
+            }

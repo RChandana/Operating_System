378210f1df83e04a2cb369173ee90f9fcce14f82
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2023-01-01 13:33:50 +0530

    Update Page_Replacement_Optimal.java

--- Page_Replacement_Optimal.java
+++ Page_Replacement_Optimal.java
@@ -1,1 +1,11 @@
-
+import java.util.*;
+public class Optimal_Page_Replacement{
+    static int search(int key, int frame[], int frame_size){
+        int x = 0;
+        for(int i = 0; i < frame_size; i++){
+            if(key == frame[i]){
+                x = 1;
+            }
+        }
+        return x;
+    }
